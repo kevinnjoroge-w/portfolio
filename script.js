636aa3af72aa459c3805fb7a9e609c7817f8dada
kevinnjoroge-wanjiku <kevinnjorogewanjiku@gmail.com>
2025-05-01 03:03:36 +0300

    bugs

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ document.querySelector('a[href^="#"]').forEach(anchor=> {
 })
 
 // Toggle navigation for mobile
+const contactForm = document.querySelector('form');
+        if (contactForm) {
+            contactForm.addEventListener('submit', function(e) {
+                e.preventDefault();
+                
+                // Here you would typically send the form data to your server
+                // For this example, we'll just show an alert
+                alert('Thank you for your message! I will get back to you soon.');
+                this.reset();
+            });
+        }
